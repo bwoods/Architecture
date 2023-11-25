e47679adf48a0f72eeff27f4b60267412449f843
Bryan Woods <woods.bryan@gmail.com>
2023-11-24 20:08:39 -0800

    Adding support for async `Reducer`s

--- src/reducer/mod.rs
+++ src/reducer/mod.rs
@@ -1,3 +1,5 @@
+use std::future::Future;
+
 use crate::effects::Effects;
 
 #[doc = include_str!("README.md")]
@@ -5,8 +7,20 @@ pub trait Reducer {
     /// …
     type Action;
 
+    #[allow(unused_variables)]
     /// …
-    fn reduce(&mut self, action: Self::Action, effects: impl Effects<Action = Self::Action>);
+    fn reduce(&mut self, action: Self::Action, effects: impl Effects<Action = Self::Action>) {}
+
+    #[inline(always)]
+    fn reduce_async(
+        &mut self,
+        action: Self::Action,
+        effects: impl Effects<Action = Self::Action>,
+    ) -> impl Future<Output = ()> {
+        async {
+            self.reduce(action, effects);
+        }
+    }
 
     /// …
     type Output;
--- src/store/runtime.rs
+++ src/store/runtime.rs
@@ -36,10 +36,15 @@ impl<State: Reducer> Store<State> {
                         while let Ok(result) = receiver.recv_async().await {
                             match result {
                                 Ok(action) => {
-                                    state.reduce(action, effects.clone());
+                                    state.reduce_async(action, effects.clone()).await;
 
-                                    while let Some(action) = effects.borrow_mut().pop_front() {
-                                        state.reduce(action, effects.clone());
+                                    // wrapping the `borrow_mut` in a closure to ensure the borrow
+                                    // is dropped before the `await` that follows
+                                    let next = || effects.borrow_mut().pop_front();
+
+                                    // see: https://rust-lang.github.io/rust-clippy/master/index.html#await_holding_refcell_ref
+                                    while let Some(action) = next() {
+                                        state.reduce_async(action, effects.clone()).await;
                                     }
                                 }
                                 Err(parked) => {
