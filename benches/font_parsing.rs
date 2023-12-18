fe884de64d3063cf3f95a819ba65d17c5474878f
Bryan Woods <woods.bryan@gmail.com>
2023-12-17 17:26:16 -0800

    Removing fontdb

--- benches/font_parsing.rs
+++ benches/font_parsing.rs
@@ -15,7 +15,7 @@ fn main() {
 fn font_face_parsing() -> f32 {
     with_default_font(|| {
         let inter: Dependency<Inter> = Default::default();
-        inter.with_face(400.0, 14.0, |_face, scale| black_box(scale))
+        inter.with(400.0, 14.0, |_face, scale| black_box(scale))
     })
     .unwrap()
 }
--- src/views/minimal/font/mod.rs
+++ src/views/minimal/font/mod.rs
@@ -1,77 +1,54 @@
 mod typography;
 
-use std::cell::RefCell;
-use std::sync::Arc;
+use rustybuzz::{ttf_parser::fonts_in_collection, Face, Tag};
 
-use fontdb::{Database, Source, ID};
-use rustybuzz::{Face, Tag};
-
-use crate::dependencies::{Dependency, DependencyDefault};
-
-#[derive(Default)]
-pub(crate) struct Repository(RefCell<Database>);
+use crate::dependencies::{with_dependency, Dependency, DependencyDefault};
 
 ///
-pub struct Inter(ID);
+pub struct Inter<'a>(&'a [u8]);
 
-impl Inter {
+impl Inter<'_> {
     ///
     #[inline(never)]
-    pub fn with_face<F, R>(&self, weight: f32, size: f32, f: F) -> Option<R>
+    pub fn with<F, R>(&self, weight: f32, size: f32, f: F) -> Option<R>
     where
         F: FnOnce(&Face, f32) -> R,
     {
         let opsz = size.clamp(14.0, 32.0);
         let wght = weight.clamp(100.0, 900.0);
+        let face_index = 0;
 
-        let db: Dependency<Repository> = Default::default();
-        let result =
-            db.0.borrow()
-                .with_face_data(self.0, move |data, face_index| {
-                    Face::from_slice(data, face_index).as_mut().map(|face| {
-                        face.set_variation(Tag::from_bytes(b"opsz"), opsz);
-                        face.set_variation(Tag::from_bytes(b"wght"), wght);
+        Face::from_slice(self.0, face_index).as_mut().map(|face| {
+            face.set_variation(Tag::from_bytes(b"opsz"), opsz);
+            face.set_variation(Tag::from_bytes(b"wght"), wght);
 
-                        let scale = size / face.units_per_em() as f32;
-                        f(face, scale)
-                    })
-                })
-                .flatten();
-
-        result
+            let scale = size / face.units_per_em() as f32;
+            f(face, scale)
+        })
     }
 }
 
-impl Default for Inter {
+impl Default for Inter<'_> {
     fn default() -> Self {
-        let db: Dependency<Repository> = Default::default();
-        let source = Source::Binary(Arc::new(include_bytes!("InterVariable.ttf")));
+        let data = include_bytes!("InterVariable.ttf");
+        let n = fonts_in_collection(data).unwrap_or(1);
+        assert_eq!(n, 1); // not a font collection
 
-        let ids = db.0.borrow_mut().load_font_source(source);
-        assert!(ids.len() == 1); // not a font collection
-
-        Self(ids[0])
+        Self(data)
     }
 }
 
-impl DependencyDefault for Repository {}
-impl DependencyDefault for Inter {}
+impl DependencyDefault for Inter<'_> {}
 
 ///
 pub fn with_default_font<F: FnOnce() -> R, R>(f: F) -> R {
-    use crate::dependencies::with_dependency;
-
-    // Repository must be registered before Inter::default() is called or else the
-    // “using `DependencyDefault` is not allowed in a unit test” will be triggered.
-    with_dependency(Repository::default(), || {
-        with_dependency(Inter::default(), f)
-    })
+    with_dependency(Inter::default(), f)
 }
 
 #[test]
 fn test_font_defaults() {
     with_default_font(|| {
         let inter: Dependency<Inter> = Default::default();
-        assert_ne!(inter.0, ID::dummy());
+        assert!(inter.is_some());
     });
 }
