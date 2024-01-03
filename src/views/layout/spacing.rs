1af2016676b85005393632c7d250383fd6190db2
Bryan Woods <woods.bryan@gmail.com>
2024-01-02 20:32:04 -0800

    Rebuilding `Spacer` around `OnceCell`

--- src/views/layout/spacing.rs
+++ src/views/layout/spacing.rs
@@ -1,28 +1,48 @@
+use std::cell::OnceCell;
+
 use crate::views::{Bounds, Event, Output, Point, Size, View};
 
-pub struct Empty;
-
-impl View for Empty {
-    #[inline(always)]
-    fn size(&self) -> Size {
-        Size::zero()
-    }
-    #[inline(always)]
-    fn event(&self, _event: Event, _offset: Point, _bounds: Bounds) {}
-    #[inline(always)]
-    fn draw(&self, _bounds: Bounds, _onto: &mut impl Output) {}
-}
-
-pub struct Spacer(pub(crate) std::cell::Cell<Size>);
+pub struct Spacer(pub(crate) OnceCell<Size>);
 
 impl Spacer {
     #[inline(always)]
-    pub fn empty() -> Empty {
-        Empty
+    pub fn fill() -> Self {
+        Spacer(OnceCell::new()) // a flexible spacer has no size (yet)
     }
 
     #[inline(always)]
-    pub fn fill() -> Self {
-        Spacer(Default::default())
+    pub fn fixed(width: f32, height: f32) -> Self {
+        let spacer = Self::fill();
+        spacer.0.set(Size::new(width, height)).ok();
+        spacer
     }
+
+    #[inline(always)]
+    pub fn width(width: f32) -> Self {
+        Spacer::fixed(width, 0.0)
+    }
+
+    #[inline(always)]
+    pub fn height(height: f32) -> Self {
+        Spacer::fixed(0.0, height)
+    }
+
+    #[inline(always)]
+    pub fn empty() -> Self {
+        Self::fixed(0.0, 0.0)
+    }
+}
+
+#[allow(unused_variables)]
+impl View for Spacer {
+    #[inline]
+    fn size(&self) -> Size {
+        self.0.get().cloned().unwrap_or_default()
+    }
+
+    #[inline(always)]
+    fn event(&self, event: Event, offset: Point, bounds: Bounds) {}
+
+    #[inline(always)]
+    fn draw(&self, bounds: Bounds, onto: &mut impl Output) {}
 }
