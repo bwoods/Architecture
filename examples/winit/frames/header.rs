0f3d8e58d4def6557d519e11dc43969e6a74d0bd
Bryan Woods <woods.bryan@gmail.com>
2024-02-02 20:43:57 -0800

    Further `minimal` views usage

---
+++ examples/winit/frames/header.rs
@@ -0,0 +1,34 @@
+use composable::views::ui::spacing;
+use composable::{views::*, Effects, Reducer};
+
+#[derive(Debug, Default)]
+pub struct State {}
+
+#[derive(Clone, Debug)]
+pub enum Action {}
+
+impl Reducer for State {
+    type Action = Action;
+    type Output = Self;
+
+    fn reduce(&mut self, action: Action, _effects: impl Effects<Action>) {
+        match action {}
+    }
+}
+
+impl State {
+    pub fn view(&self, effects: impl Effects<Action>) -> impl View {
+        (
+            Rectangle {
+                rgba: [246, 248, 250, 255],
+            }
+            .fill()
+            .height(spacing::XL),
+            Rectangle {
+                rgba: [217, 222, 227, 255],
+            }
+            .fill()
+            .height(1.0),
+        )
+    }
+}
--- examples/winit/frames/mod.rs
+++ examples/winit/frames/mod.rs
@@ -1,27 +1,29 @@
+use composable::{dependencies::with_dependency, views::View, Effects, From, Reducer, TryInto};
+
+use crate::{wgpu, window};
+
 mod header;
 
-use composable::dependencies::with_dependency;
-use composable::*;
-
-use crate::wgpu;
-use crate::window;
-
-pub(crate) struct State {
+pub struct State {
     wgpu: wgpu::Surface,
     proxy: window::EventLoopProxy,
+
+    header: header::State,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, From, TryInto)]
 pub enum Action {
     Resize { width: u32, height: u32 },
     Redraw,
+
+    Header(header::Action),
 }
 
 impl Reducer for State {
     type Action = Action;
     type Output = Self;
 
-    fn reduce(&mut self, action: Self::Action, effects: impl Effects<Self::Action>) {
+    fn reduce(&mut self, action: Action, effects: impl Effects<Action>) {
         match action {
             Action::Resize { width, height } => {
                 self.wgpu.resize(width, height);
@@ -29,12 +31,24 @@ impl Reducer for State {
             Action::Redraw => with_dependency(self.wgpu.transform(), || {
                 self.wgpu.render().ok();
             }),
+            Action::Header(_) => {
+                //
+            }
         }
     }
 }
 
 impl State {
     pub fn new(wgpu: wgpu::Surface, proxy: window::EventLoopProxy) -> Self {
-        Self { wgpu, proxy }
+        Self {
+            wgpu,
+            proxy,
+
+            header: Default::default(),
+        }
+    }
+
+    pub fn view(&self, effects: impl Effects<Action>) -> impl View {
+        (self.header.view(effects.scope()),)
     }
 }
