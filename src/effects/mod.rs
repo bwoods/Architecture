abeabfcc3de1d56aa5d37c0d8dcbfeec17c91e98
Bryan Woods <woods.bryan@gmail.com>
2024-07-06 03:46:38 -0700

    `Scheduler` functions for `Effects`

--- src/effects/mod.rs
+++ src/effects/mod.rs
@@ -1,22 +1,25 @@
 #![doc = include_str!("README.md")]
 
 use std::cell::RefCell;
+use std::cmp::max;
 use std::collections::VecDeque;
 use std::marker::PhantomData as Marker;
 use std::rc::Weak;
 use std::time::{Duration, Instant};
 
-use futures::{future, stream::once, Future, Stream, StreamExt};
-
 use crate::effects::scheduler::Delay;
 pub(crate) use crate::effects::task::Executor;
 #[doc(hidden)]
 pub use crate::effects::task::Task;
+use futures::stream::unfold;
+use futures::{future, stream::once, Future, Stream, StreamExt};
 
 mod scheduler;
 mod task;
 
 /// `Effects` are used within `Reducer`s to propagate `Action`s as side-effects of performing other `Action`s.
+///
+/// `Effects` are also [`Scheduler`]s; able to apply modifiers to when (and how often) `Action`s. are sent.
 ///
 /// See [the module level documentation](self) for more.
 pub trait Effects: Clone {
@@ -35,43 +38,6 @@ pub trait Effects: Clone {
     /// while it is running. Otherwise [`future`][Effects::future] or [`stream`][Effects::stream]
     /// should be preferred.
     fn task<S: Stream<Item = Self::Action> + 'static>(&self, stream: S) -> Task;
-
-    /// An effect that sends an [`Action`][`Self::Action`] through the `Store`’s
-    /// [`Reducer`][`crate::Reducer`] if at least `delay` time has passed since
-    /// `previous` was sent. Otherwise, all subsequent actions but the last are
-    /// dropped until that time; which resets the countdown until the next
-    /// debounced action can be sent.
-    ///
-    /// The `debounce` function will automatically update the information
-    /// stored in `previous` as it runs. The `Task` configured for this call
-    /// will be the _previous_ task for the next call, after all.
-    fn debounce(
-        &self,
-        action: impl Into<Self::Action> + 'static,
-        previous: &mut Option<Task>,
-        delay: Duration,
-    ) where
-        Self::Action: 'static,
-    {
-        let now = Instant::now(); // FIXME: get from the Scheduler?
-        let when = match previous.take().and_then(|task| task.when) {
-            Some(when) if when > now => when, // previous was not yet sent — replace it
-            Some(when) if when + delay > now => when + delay, // previous was sent recently; delay this send
-            _ => now, // goes through the same code path as delayed events for CONSISTENT performance
-        };
-
-        let task = Task {
-            handle: self
-                .task(once(async move {
-                    Delay::new(when).await;
-                    action.into()
-                }))
-                .handle,
-            when: Some(when),
-        };
-
-        *previous = Some(task);
-    }
 
     /// An effect that runs a [`Future`][`std::future`] and, if it returns an
     /// [`Action`][`Self::Action`], sends it through the `Store`’s [`Reducer`][`crate::Reducer`].
@@ -156,5 +122,142 @@ impl<Action: 'static> Effects for Weak<RefCell<VecDeque<Action>>> {
 
     fn task<S: Stream<Item = Action> + 'static>(&self, stream: S) -> Task {
         Task::new(stream)
+    }
+}
+
+/// `Effects` are also [`Scheduler`]s; able to apply modifiers to when (and how often) `Action`s. are sent.
+pub trait Scheduler {
+    /// The [`Effects::Action`].
+    type Action;
+
+    /// Sends the `Action` after this `Instant` of time.
+    fn after(&self, instant: Instant, action: impl Into<Self::Action> + 'static) -> Task;
+
+    /// Sends the `Action` every `Interval` of time.
+    fn every(&self, interval: Interval, action: impl Into<Self::Action> + Clone + 'static) -> Task;
+
+    /// An effect that sends an [`Action`][`Self::Action`] through the `Store`’s
+    /// [`Reducer`][`crate::Reducer`] if at least one `interval` of time has passed
+    /// since `previous` was sent. Otherwise, all subsequent actions but the last
+    /// are dropped until that time; which resets the countdown until the next
+    /// debounced action can be sent.
+    ///
+    /// The `debounce` function will automatically update the information
+    /// stored in `previous` as it runs. The `Task` debounced by this call
+    /// will be the _previous_ task for the next call, if any.
+    fn debounce(
+        &self,
+        action: impl Into<Self::Action> + 'static,
+        previous: &mut Option<Task>,
+        interval: Interval,
+    ) where
+        Self::Action: 'static;
+
+    /// An effect that coalesces repeated attempts to send [`Action`][`Self::Action`]s
+    /// through the`Store`’s [`Reducer`][`crate::Reducer`] into a singe send.
+    /// Once `timeout` has elapsed with no further actions being attempted, a single
+    /// `Action` will be sent.
+    ///
+    /// The `coalesce` function will automatically update the information
+    /// stored in `previous` as it runs. The `Task` coalesced by this call
+    /// will be the _previous_ task for the next call, if any.
+    fn coalesce(
+        &self,
+        action: impl Into<Self::Action> + 'static,
+        previous: &mut Option<Task>,
+        timeout: Duration,
+    ) where
+        Self::Action: 'static;
+}
+
+impl<T: Effects> Scheduler for T {
+    type Action = T::Action;
+
+    fn after(&self, instant: Instant, action: impl Into<Self::Action> + 'static) -> Task {
+        self.task(once(async move {
+            Delay::new(instant).await;
+            action.into()
+        }))
+    }
+
+    fn every(&self, interval: Interval, action: impl Into<Self::Action> + Clone + 'static) -> Task {
+        let (n, delay) = match interval {
+            Interval::Leading(delay) => (0, delay), // 0 × delay => no initial delay
+            Interval::Trailing(delay) => (1, delay),
+        };
+
+        let start = Instant::now(); // FIXME
+        self.task(unfold(n, move |n| {
+            let action = action.clone();
+
+            async move {
+                let instant = start.checked_add(delay.checked_mul(n)?)?;
+                Delay::new(instant).await;
+
+                Some((action.into(), n + 1))
+            }
+        }))
+    }
+
+    fn debounce(
+        &self,
+        action: impl Into<Self::Action> + 'static,
+        previous: &mut Option<Task>,
+        interval: Interval,
+    ) where
+        Self::Action: 'static,
+    {
+        let now = Instant::now(); // FIXME
+        let delay = interval.duration();
+
+        let when = match previous.take().and_then(|task| task.when) {
+            Some(when) if when > now => when, // previous was not yet sent — replace it
+            Some(when) if when + delay > now => when + delay, // previous was sent recently; delay this send
+            _ => match interval {
+                Interval::Leading(_) => now,
+                Interval::Trailing(_) => now + delay,
+            },
+        };
+
+        let task = self.after(when, action);
+        *previous = Some(task);
+    }
+
+    fn coalesce(
+        &self,
+        action: impl Into<Self::Action> + 'static,
+        waiting: &mut Option<Task>,
+        timeout: Duration,
+    ) where
+        Self::Action: 'static,
+    {
+        let now = Instant::now(); // FIXME
+
+        let when = waiting
+            .take()
+            .and_then(|task| task.when)
+            .map(|previous| max(previous + timeout, now))
+            .unwrap_or_else(|| now + timeout);
+
+        let task = self.after(when, action);
+        *waiting = Some(task);
+    }
+}
+
+/// When a [`Scheduler`] uses a repeating interval, that interval can begin immediately, a `Leading`
+/// interval, or it may begin after the first delay, a `Trailing` interval.
+pub enum Interval {
+    /// The first `Action` should be sent immediately.
+    Leading(Duration),
+    /// The first `Action` should not be send until after the first `Interval` has passed.
+    Trailing(Duration),
+}
+
+impl Interval {
+    pub fn duration(&self) -> Duration {
+        match self {
+            Interval::Leading(duration) => *duration,
+            Interval::Trailing(duration) => *duration,
+        }
     }
 }
--- src/effects/scheduler.rs
+++ src/effects/scheduler.rs
@@ -1,7 +1,7 @@
 use std::cmp::Reverse;
 use std::collections::VecDeque;
 use std::future::Future;
-use std::mem::{replace, swap};
+use std::mem::replace;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
@@ -10,21 +10,10 @@ use std::time::Instant;
 
 use crate::dependencies::{Dependency, DependencyDefault};
 
-#[derive(Default)]
 enum State {
-    #[default]
-    None,
-    Pending(Waker),
-    Ready(Instant),
-}
-
-impl State {
-    fn is_some(&self) -> bool {
-        match self {
-            State::None => false,
-            _ => true,
-        }
-    }
+    New(Instant),
+    Waiting(Waker),
+    Ready,
 }
 
 #[derive(Clone)]
@@ -32,81 +21,59 @@ pub struct Delay(Arc<Mutex<State>>);
 
 impl Delay {
     pub fn new(instant: Instant) -> Self {
-        let delay = Delay(Default::default());
-
-        let scheduler = Dependency::<Scheduler>::new();
-        scheduler.add(instant, delay.0.clone());
-
-        delay
+        Delay(Arc::new(Mutex::new(State::New(instant))))
     }
 }
 
-/// When `await`ed as a `Future` it returns the `Instant` it scheduled for —
-/// which may not necessarily be the same `Instant` it was actually returned.
 impl Future for Delay {
-    type Output = Instant;
+    type Output = ();
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut state = self.0.lock().unwrap();
-        let state = &mut *state;
+        let mut state = self.0.lock().unwrap_or_else(|err| err.into_inner());
 
-        match state {
-            State::None => {
-                swap(state, &mut State::Pending(cx.waker().clone()));
+        match &mut *state {
+            State::New(instant) => {
+                let instant = *instant;
+                *state = State::Waiting(cx.waker().clone());
+                drop(state);
+
+                // Now that it has a Waker…
+                let scheduler = Dependency::<Scheduler>::new();
+                scheduler.add(instant, self.0.clone());
+
                 Poll::Pending
             }
-            State::Pending(waker) => {
+            State::Waiting(waker) => {
                 waker.clone_from(cx.waker()); // update the waker if needed
                 Poll::Pending
             }
-            State::Ready(now) => Poll::Ready(*now),
+            State::Ready => Poll::Ready(()),
         }
     }
 }
 
 /// Shared between the `Scheduler` and its polling Thread (if any)
+#[derive(Default)]
 struct Shared {
     queue: Queue<Instant, Arc<Mutex<State>>>,
-    now: Option<Instant>,
-}
-
-impl Default for Shared {
-    fn default() -> Self {
-        Self {
-            queue: Default::default(),
-            now: None,
-        }
-    }
 }
 
 struct Scheduler {
     shared: Arc<Mutex<Shared>>,
-    handle: Option<JoinHandle<()>>,
+    handle: JoinHandle<()>,
 }
 
 impl Scheduler {
+    #[inline(never)]
     pub fn add(&self, new: Instant, state: Arc<Mutex<State>>) {
         let mut shared = self.shared.lock().unwrap();
         let next = shared.queue.peek_next();
         shared.queue.insert(new, state);
         drop(shared);
 
-        self.reschedule_polling_thread_if_needed(new, next);
-    }
-
-    pub(crate) fn advance_to(&self, now: Instant) {
-        let mut shared = self.shared.lock().unwrap();
-        let next = shared.queue.peek_next();
-        shared.now = Some(now);
-        drop(shared);
-
-        self.reschedule_polling_thread_if_needed(now, next);
-    }
-
-    fn reschedule_polling_thread_if_needed(&self, new: Instant, next: Option<Instant>) {
-        match (&self.handle, next) {
-            (Some(polling), None) => polling.thread().unpark(), // no `unpark` is scheduled yet
-            (Some(polling), Some(pending)) if new < pending => polling.thread().unpark(),
+        match next {
+            None => self.handle.thread().unpark(), // no `unpark` is scheduled yet
+            Some(pending) if new < pending => self.handle.thread().unpark(),
             _ => {}
         }
     }
@@ -118,42 +85,35 @@ impl Default for Scheduler {
         let shared = Arc::new(Mutex::<Shared>::default());
         let remote = shared.clone();
 
-        let handle = Some(
-            Builder::new()
-                .name(std::any::type_name::<Self>().into())
-                .spawn(move || {
-                    // thread loop ends if the `Mutex` is poisoned
-                    while let Ok(mut shared) = remote.lock() {
-                        let now = shared.now.take().unwrap_or_else(|| Instant::now());
-                        let delayed = shared.queue.drain_until_while(now, |delay| {
-                            delay // stop at un-polled Futures as they do not have a Waker yet
-                                .try_lock() // (including being polled right now!)
-                                .map(|state| state.is_some())
-                                .ok()
-                                .unwrap_or(false)
-                        });
-                        let next = shared.queue.peek_next();
-                        drop(shared); // release the `Mutex` in case any of the delayed work wants the `Scheduler`
+        let handle = Builder::new()
+            .name(std::any::type_name::<Self>().into())
+            .spawn(move || {
+                loop {
+                    let now = Instant::now();
 
-                        for (when, delay) in delayed {
-                            let mut state = delay.lock().unwrap();
-                            let waker = replace(&mut *state, State::Ready(when));
-                            drop(state); // release the `Mutex` before the waker is called
+                    let mut shared = remote.lock().unwrap();
+                    let delays = shared.queue.drain_until(now);
+                    let next = shared.queue.peek_next();
+                    drop(shared); // release the `Mutex` in case any of the delayed work wants the `Scheduler`
 
-                            match waker {
-                                State::Pending(waker) => waker.wake(),
-                                _ => unreachable!(),
-                            }
-                        }
+                    for delay in delays {
+                        let mut state = delay.lock().unwrap();
+                        let waiting = replace(&mut *state, State::Ready);
+                        drop(state); // release the `Mutex` before the waker is called
 
-                        match next {
-                            None => park(),
-                            Some(when) => park_timeout(when - now),
+                        match waiting {
+                            State::Waiting(waker) => waker.wake(),
+                            _ => unreachable!(),
                         }
                     }
-                })
-                .expect("scheduler thread"),
-        );
+
+                    match next {
+                        None => park(),
+                        Some(when) => park_timeout(when - now),
+                    }
+                }
+            })
+            .expect("scheduler thread");
 
         Self { shared, handle }
     }
@@ -173,9 +133,9 @@ impl<Key, Value> Default for Queue<Key, Value> {
     }
 }
 
-impl<Key: Clone, Value> Queue<Key, Value> {
+impl<Key: Copy, Value> Queue<Key, Value> {
     pub fn peek_next(&self) -> Option<Key> {
-        self.deque.back().map(|kv| kv.0 .0.clone())
+        self.deque.back().map(|kv| kv.0 .0)
     }
 }
 
@@ -186,19 +146,9 @@ impl<Key: PartialOrd, Value> Queue<Key, Value> {
         self.deque.insert(index, (key, value));
     }
 
-    pub fn drain_until_while(
-        &mut self,
-        key: Key,
-        mut pred: impl FnMut(&Value) -> bool,
-    ) -> impl Iterator<Item = (Key, Value)> {
-        let key = Reverse(key);
-        // without the use of `Reverse` for the keys `split_off` would return the wrong half!
-        // similarly we have to reverse the predicate…
-        let index = self.deque.partition_point(|x| x.0 < key || !pred(&x.1));
-        self.deque
-            .split_off(index)
-            .into_iter()
-            .rev()
-            .map(|kv| (kv.0 .0, kv.1))
+    pub fn drain_until(&mut self, key: Key) -> impl Iterator<Item = Value> {
+        let key = Reverse(key); // without the use of `Reverse` for the keys `split_off` would return the wrong half
+        let index = self.deque.partition_point(|x| x.0 < key);
+        self.deque.split_off(index).into_iter().rev().map(|kv| kv.1)
     }
 }
