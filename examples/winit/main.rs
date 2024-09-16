0e11e53936b34696377995c126203761875783e0
Bryan Woods <woods.bryan@gmail.com>
2024-09-16 13:01:04 -0700

    Updating winit
    Some part of the refactor has broken `muda` on Win32.
    
    At least in Wine.

--- examples/winit/main.rs
+++ examples/winit/main.rs
@@ -2,58 +2,109 @@
 #![allow(unused_variables)]
 
 use futures::executor::block_on;
+use std::collections::BTreeMap;
 use std::sync::Arc;
 
 use composable::Store;
+use window::Action;
+
+use window::menubar::Menu;
 use window::{
-    Action, ControlFlow, Event, EventLoopError, LogicalSize, Size, StartCause, WindowEvent,
+    ActiveEventLoop, ApplicationHandler, ControlFlow, DeviceEvents, EventLoopError, EventLoopProxy,
+    LogicalSize, Size, StartCause, Window, WindowEvent, WindowId,
 };
 
 mod frames;
 mod wgpu;
 mod window;
 
+struct State {
+    stores: BTreeMap<WindowId, (Store<frames::State>, Arc<Window>)>,
+
+    proxy: EventLoopProxy,
+    menubar: Menu,
+}
+
+impl State {
+    fn front_window(&self) -> Option<&Window> {
+        self.stores
+            .values()
+            .map(|tuple| &*tuple.1)
+            .find(|window| window.has_focus())
+    }
+}
+
+impl ApplicationHandler<Action> for State {
+    fn new_events(&mut self, event_loop: &ActiveEventLoop, cause: StartCause) {
+        match cause {
+            StartCause::Init => self.proxy.send_event(Action::NewWindow).unwrap(),
+            _ => {}
+        }
+    }
+
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {}
+
+    fn user_event(&mut self, active: &ActiveEventLoop, event: Action) {
+        match event {
+            Action::NewWindow => {
+                let window = Arc::new(window::build(&active));
+                window::menubar::attach(&self.menubar, &window);
+
+                let id = window.id();
+                let proxy = self.proxy.clone();
+                let wgpu = block_on(wgpu::Surface::new(window.clone())); // must be on main thread
+
+                let store = Store::new(move || frames::State::new(wgpu, proxy, id));
+                self.stores.insert(id, (store, window));
+            }
+            Action::DefaultSize => {
+                if let Some(window) = self.front_window() {
+                    let size = Size::from(LogicalSize::<f32>::from(window::DEFAULT_SIZE));
+                    let _ = window.request_inner_size(size);
+                }
+            }
+        }
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::RedrawRequested => {
+                self.stores
+                    .entry(id)
+                    .and_modify(|store| store.0.send(frames::Action::Redraw));
+            }
+            WindowEvent::Resized(size) => {
+                let (width, height) = size.into();
+                let resize = frames::Action::Resize { width, height };
+                self.stores.entry(id).and_modify(|store| {
+                    store.0.send(resize);
+                    store.1.request_redraw();
+                });
+            }
+            WindowEvent::CloseRequested => {
+                self.stores.remove(&id);
+                if self.stores.is_empty() {
+                    event_loop.exit();
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() -> Result<(), EventLoopError> {
-    let (window, menu, event_loop) = window::build();
-    event_loop.set_control_flow(ControlFlow::Wait); // turn off polling
-    let id = window.id();
+    let (menubar, event_loop) = window::menubar::build()?;
 
-    let window = Arc::new(window);
+    event_loop.listen_device_events(DeviceEvents::Never);
+    event_loop.set_control_flow(ControlFlow::Wait);
+
     let proxy = event_loop.create_proxy();
-    let wgpu = block_on(wgpu::Surface::new(window.clone()));
 
-    let store = Store::new(move || frames::State::new(wgpu, proxy, id));
+    let mut state = State {
+        stores: BTreeMap::default(),
+        menubar,
+        proxy,
+    };
 
-    event_loop.run(move |event, target| match event {
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
-            window_id,
-            ..
-        } => target.exit(),
-        Event::WindowEvent {
-            event: WindowEvent::Resized(size),
-            window_id,
-            ..
-        } => {
-            let (width, height) = size.into();
-            store.send(frames::Action::Resize { width, height });
-            window.request_redraw();
-        }
-        Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            window_id,
-            ..
-        } => {
-            store.send(frames::Action::Redraw);
-        }
-
-        Event::UserEvent(Action::DefaultSize) => {
-            let _ = window
-                .request_inner_size(Size::from(LogicalSize::<f32>::from(window::DEFAULT_SIZE)));
-        }
-        Event::NewEvents(StartCause::Init) => {
-            //
-        }
-        _ => {}
-    })
+    event_loop.run_app(&mut state)
 }
